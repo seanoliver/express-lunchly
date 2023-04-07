2fa55fd591748a9b8b13244634a6beed788dda04
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-04-07 11:49:35 -0700

    added customer full name method and updated templates to reflect

--- models/customer.js
+++ models/customer.js
@@ -90,6 +90,12 @@ class Customer {
       );
     }
   }
+
+  /** Gets a full name string for a customer. */
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
 }
 
 module.exports = Customer;
